3e069c58eaeeeebb533ceaaf347ea57fe41094a4
Arulloa <Ar.ulloa@duocuc.cl>
2026-06-28 19:36:13 -0400

    Delete bitacoraRepository.java

--- bitacoraRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.api_triage.Repository;
-
-import cl.duoc.api_triage.Model.bitacora;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface bitacoraRepository extends JpaRepository<bitacora, Long> {
-}
