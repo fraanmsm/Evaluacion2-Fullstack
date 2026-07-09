bd57db12178e39fc35d9251ab6a07ebff8d77c74
Arulloa <Ar.ulloa@duocuc.cl>
2026-07-09 17:51:37 -0400

    Create JwtUtil.java
    Se crea un generador de tokens

---
+++ api-gateway/api-gateway/src/main/java/cl/duoc/api_gateway/security/JwtUtil.java
@@ -0,0 +1,27 @@
+package cl.duoc.api_gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generarToken(String usuario) {
+        return Jwts.builder()
+                .setSubject(usuario)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+
+    public void validarToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+    }
+}
